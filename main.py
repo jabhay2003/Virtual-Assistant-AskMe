cc5dae04516b15f0d9af644e5c3b3275c1d98ef7
jabhay2003 <163547129+jabhay2003@users.noreply.github.com>
2025-12-17 10:01:15 +0530

    Implement speech recognition and text-to-speech functionality

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
 if __name__ == "__main__":
-    pass
+    speak("Initializing AskMe...")
+    while True:
+        # Listen for the wake word "AskMe"
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source)
+        print("recognizing...")
+        try:
+            command = r.recognize_google(audio)
+            print(command)
+        except sr.UnknownValueError:
+             print("Could not understand audio")
